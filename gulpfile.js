73b34eb2daa6eca8f0570dc04143c4eb9dc9d0d5
maksym-rakomin <49858760+maksym-rakomin@users.noreply.github.com>
2019-11-16 12:44:45 +0200

    Изменена структура проекта

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,40 @@ var rigger       = require('gulp-rigger');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("scss/*.scss")
+    return gulp.src("src/scss/*.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             cascade: false
         }))
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('html', function() {
     return gulp.src([
-        'partials/*.html',
-        '!partials/_*.html'])
+        'src/**/*.html',
+        '!src/**/_*.html'])
         .pipe(rigger())
-        .pipe(gulp.dest("./"))
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*.*')
+        .pipe(gulp.dest("dist/img"))
         .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
-gulp.task('serve', gulp.series('html', 'sass', function() {
+gulp.task('serve', gulp.series('html', 'img', 'sass', function() {
 
     browserSync.init({
-        server: "./"
+        server: "./dist"
     });
 
-    gulp.watch("scss/*.scss", gulp.parallel('sass'));
-    gulp.watch("partials/*.html", gulp.parallel('html'));
-    gulp.watch("js/*.js").on('change', browserSync.reload);
+    gulp.watch("src/scss/*.scss", gulp.parallel('sass'));
+    gulp.watch("src/**/*.html", gulp.parallel('html'));
+    gulp.watch("src/img/**/*.*", gulp.parallel('img'));
 }));
 
 
